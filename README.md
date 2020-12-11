2f78f65fc14ed08a9fedf2b5f0be213218da19c5
struthj2 <struthjoseph@gmail.com>
2020-12-11 13:43:12 -0800

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 This is a repo for hello world stuff.
 
 This is the composer branch.
+
+# Awknowledgements
+Credit to
+- HelloWorld from: https://www.frobiovox.com/posts/2016/08/16/basic-hello-world-with-composer-and-php.html
+- ComposerParser from: https://github.com/MCStreetguy/ComposerParser
+# php-composer-parse-example
--- src/helloworld/greetings.php
+++ src/helloworld/greetings.php
@@ -2,11 +2,20 @@
 namespace HelloWorld;
 
 use PHP_Timer;
+use MCStreetguy\ComposerParser\Factory as ComposerParser;
 
 class Greetings {
   public static function sayHelloWorld() {
     $timer = new PHP_Timer();
     $timer->start();
+    $composerJson = ComposerParser::parse('/Users/joseph/Documents/ComposerParser/composer.json');
+    $lockfile = ComposerParser::parse('/Users/joseph/Documents/ComposerParser/composer.lock');
+
+    print_r($composerJson);
+
+    print_r($lockfile);
     return "Hello World\n" . $timer->resourceUsage() . "\n";
+
+
   }
 }
